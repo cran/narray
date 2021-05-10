916fc5cb4c5d3f110765c262270b2f878c36e432
Michael Schubert <mschu.dev@gmail.com>
2021-05-10 12:10:08 +0000

    version 0.4.1.1

--- inst/doc/narray.R
+++ inst/doc/narray.R
@@ -15,12 +15,12 @@ opts_chunk$set(
 
 library(narray)
 
-## ----namespace, eval=FALSE-----------------------------------------------
+## ----namespace, eval=FALSE----------------------------------------------------
 #  # example referencing the package namespace
 #  # do not load the package with 'library(...)' here
 #  narray::stack(...)
 
-## ----stack---------------------------------------------------------------
+## ----stack--------------------------------------------------------------------
 A = matrix(1:4, nrow=2, ncol=2, dimnames=list(c('a','b'),c('x','y')))
 B = matrix(5:6, nrow=2, ncol=1, dimnames=list(c('b','a'),'z'))
 
@@ -30,19 +30,19 @@ C
 D = stack(m=A, n=C, along=3) # we can also introduce new dimensions
 D
 
-## ----split---------------------------------------------------------------
+## ----split--------------------------------------------------------------------
 split(C, along=2, subsets=c('s1','s1','s2'))
 
-## ----map-----------------------------------------------------------------
+## ----map----------------------------------------------------------------------
 map(C, along=2, function(x) x*2) # return same length vector
 map(C, along=2, mean, subsets=c('s1', 's1', 's2')) # summarize each subset to scalar
 
-## ----lambda--------------------------------------------------------------
+## ----lambda-------------------------------------------------------------------
 dot = function(x, y) sum(x * y)
 lambda(~ dot(A, B), along=c(A=1, B=2))
 lambda(~ dot(A, B), along=c(A=1, B=2), simplify=FALSE)
 
-## ----intersect-----------------------------------------------------------
+## ----intersect----------------------------------------------------------------
 E = matrix(1:6, nrow=3, dimnames=list(c('a','b','d'), c('x','y')))
 F = matrix(7:9, nrow=3, dimnames=list(c('b','a','c'), 'z'))
 
@@ -50,12 +50,12 @@ intersect(E, F, along=1)
 E
 F
 
-## ----construct-----------------------------------------------------------
+## ----construct----------------------------------------------------------------
 DF = data.frame(k1=base::rep(letters[1:3],2),
                 k2=base::rep(letters[24:25],3), v=1:6)[-6,]
 construct(v ~ k1 + k2, data=DF)
 
-## ----mask----------------------------------------------------------------
+## ----mask---------------------------------------------------------------------
 G = list(a='e1', b=c('e1','e2'), c='e2')
 mask(G)
 
